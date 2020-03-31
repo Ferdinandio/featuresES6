ef34385abf1c4a870ab615f4b20bbb3ee9773445
Ferdinandio <dmi8829@yandex.ru>
2020-03-31 12:39:01 +0400

    last

---
+++ json/json.js
@@ -0,0 +1,11 @@
+let options = {
+	width: 1024,
+	height: 768,
+	background: 'red',
+	font: {
+		size: '14px',
+		color: '#fff'
+	}
+};
+
+console.log(JSON.parse(JSON.stringify(options)));
--- spread/spread.js
+++ spread/spread.js
@@ -1,17 +1,17 @@
-let video = ['youtube', 'vimeo', 'rutube'];
-let blogs = ['wordpress', 'livejournal', 'blogger'];
-let internet = [...video, ...blogs, 'vk', 'facebook'];
+// let video = ['youtube', 'vimeo', 'rutube'];
+// let blogs = ['wordpress', 'livejournal', 'blogger'];
+// let internet = [...video, ...blogs, 'vk', 'facebook'];
 
-console.log(internet);
+// console.log(internet);
 
 
-function log(a, b, c) {
-	console.log(a);
-	console.log(b);
-	console.log(c);
-	console.log(a + b + c);
-}
+// function log(a, b, c) {
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+// 	console.log(a + b + c);
+// }
 
-let numbers = [2, 5, 7];
+// let numbers = [2, 5, 7];
 
-log(...numbers);
+// log(...numbers);
